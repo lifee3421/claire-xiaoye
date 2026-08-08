75dea58bc5a34e2ce44dba806488b666a05103c9
lifee3421 <15611598908@163.com>
2026-08-08 10:31:21 +0800

    feat(mall): mount reward surprise and challenge panels

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import RewardShopGamePanelsHost from "./components/RewardShopGamePanelsHost.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <RewardShopGamePanelsHost />
   </React.StrictMode>
 );
